0cfd28235d1bc102fbe884c75ac68239308af5b9
Mohammmed sayrafi <127789987+Mohammedsyr98@users.noreply.github.com>
2024-06-23 15:23:17 +0300

    Delete public/src directory

--- public/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import darkModeState from "./features/darkmodeSlice";
-import boardSlice from "./features/boardSlice";
-import popupSliceMode from "./features/popupSliceMode";
-import childPopupMode from "./features/childPopupSlice";
-
-export default configureStore({
-  reducer: { darkModeState, boardSlice, popupSliceMode, childPopupMode },
-});
